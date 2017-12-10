c03b7d097b7dcf7f1bbdd239aef77cd45cba0146
Joubn <595700239@qq.com>
2017-12-10 12:06:24 +0800

    聊天室

--- wsSever.js
+++ wsSever.js
@@ -2,15 +2,29 @@ var ws = require("nodejs-websocket")
 
 
 var PORT = 80
+var clientCount = 0
 // Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
     console.log("New connection")
+    clientCount++
+    conn.nickname = 'user' + clientCount
+    var mes = {}
+    mes.type = 'enter'
+    mes.data = conn.nickname + ' comes in'
+    broadcast(JSON.stringify(mes))
     conn.on("text", function (str) {
         console.log("Received "+str)
-        conn.sendText(str.toUpperCase()+"!!!")
+        var mes = {}
+        mes.type = 'message'
+        mes.data = conn.nickname +':'+str
+        broadcast(JSON.stringify(mes))
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
+        var mes = {}
+        mes.type = 'leave'
+        mes.data = conn.nickname + ' left'
+        broadcast(JSON.stringify(mes))
     })
     conn.on("error",function(err){
         console.log('handle err')
@@ -18,3 +32,10 @@ var server = ws.createServer(function (conn) {
     })
 }).listen(PORT)
 console.log('websocket sever litstening on port '+PORT)
+
+
+function broadcast(str) {
+    server.connections.forEach(function(connection){
+        connection.sendText(str)
+    })
+}
